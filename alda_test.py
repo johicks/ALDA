19ae78f2b30ff44a0d6a38522aa3bb56fc8d1f8a
Josh Hicks <josh.hicks@live.com>
2016-12-05 14:21:17 -0800

    Finish test suite

--- alda_test.py
+++ alda_test.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import alda
+import json
 import pytest
 import requests
+from urllib.parse import urljoin
+from akamai.netstorage import Netstorage
 
 
 def test_validate_cpcodes():
@@ -92,3 +95,93 @@ def test_get_lds_configs_and_cpcodes_jp():
 
     assert len(empty_cpcode_list) == 0
     assert len(full_cpcode_list) == 3
+
+
+def test_get_ns_credentials():
+    with pytest.raises(KeyError):
+        config = alda.get_netstorage_credentials('null')
+        print(config['DEFAULT']['Hostname'])
+
+    config = alda.get_netstorage_credentials('alda.netstorage')
+    assert config['DEFAULT']['Hostname'] is not False
+    assert config['DEFAULT']['Key-name'] is not False
+    assert config['DEFAULT']['Key'] is not False
+    assert config['DEFAULT']['Cpcode'] is not False
+    assert config['DEFAULT']['Password'] is not False
+
+
+def test_create_netstorage_paths():
+    sformat = 'smooth'
+    geo = 'us'
+    cpcodes = ['123456', '789012']
+    connectionDetails = alda.get_netstorage_credentials('alda.netstorage')
+    ns = Netstorage(connectionDetails['DEFAULT']['Hostname'],
+                    connectionDetails['DEFAULT']['Key-name'],
+                    connectionDetails['DEFAULT']['Key'], ssl=True)
+    for cpcode in cpcodes:
+        ns_dir = '/{0}/logdelivery/{1}/{2}/{3}'\
+                .format(connectionDetails['DEFAULT']['Cpcode'], sformat, geo, cpcode)
+        ok, response = ns.mkdir(ns_dir)  # Test we can make directories
+        assert ok is True
+        assert response.status_code == 200
+        ok, response = ns.rmdir(ns_dir)  # Remove test directories
+        print(ok)
+        print(response.text)
+        assert ok is True
+        assert response.status_code == 200
+
+
+def test_create_lds_configs():
+    sformat = 'smooth'
+    geo = 'us'
+    cpcode = '525828'
+    openapiObj = alda.create_openapi_request('alda.edgerc', geo)
+    connectionDetails = alda.get_netstorage_credentials('alda.netstorage')
+
+    lds_payload = json.loads('''
+    {
+        "configurationType": "PRIMARY",
+        "acgObject": {
+            "id": "000000",
+            "type": "CP_CODE"
+        },
+        "productGroupId": 1,
+        "startDate": 1401840000000,
+        "logFormat": { "dictId": "2" },
+        "logIdentifier": "000000",
+        "aggregationType": "COLLECTION",
+        "deliveryType": "FTP",
+        "deliveryFrequency": {"dictId": "7"},
+        "ftpConfiguration": {
+            "directory": "",
+            "machine": "adsiuslogs.download.akamai.com",
+            "login": "amazonlogs",
+            "password": ""
+        },
+        "messageSize": { "dictId": "1" },
+        "encoding": { "dictId": "3" },
+        "contact": {
+            "contactEmail": ["amazon-dm-us-api-support@akamai.com"],
+            "dictId": "B-C-PNOHOD"
+        }
+    }''')
+    lds_payload['acgObject']['id'] = cpcode
+    lds_payload['logIdentifier'] = cpcode
+    lds_payload['ftpConfiguration']['directory'] =\
+        '/{0}/logdelivery/{1}/{2}/{3}'.format(
+        connectionDetails['DEFAULT']['Cpcode'], sformat, geo, cpcode)
+    lds_payload['ftpConfiguration']['password'] = connectionDetails['DEFAULT']['Password']
+
+    # Test that we can create a configuration
+    result = openapiObj['request'].post(urljoin(openapiObj['baseurl'], '/lds/v1/configurations'),
+                                        data=json.dumps(lds_payload),
+                                        headers={'Accept': '*/*', 'Content-Type': 'application/json'})
+    assert result.status_code == 200
+
+    resultObject = result.json()
+    serviceID = resultObject['contents']
+    # Delete the configuration we just created
+    result = openapiObj['request'].delete(urljoin(openapiObj['baseurl'], '/lds/v1/configurations/{0}'.format(serviceID)),
+                                          data=json.dumps(lds_payload),
+                                          headers={'Accept': '*/*', 'Content-Type': 'application/json'})
+    assert result.status_code == 200
